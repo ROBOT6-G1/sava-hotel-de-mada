b01d729b575a1fc78ce417f6465c3f9b339c5dc2
ROBOT6-G1 <b.outiquemevasoa@gmail.com>
2026-07-27 09:11:40 +0300

    Update firebase-config.js via DEVWEBIA

---
+++ firebase-config.js
@@ -0,0 +1,4 @@
+// DEVWEB IA - Configuration LocalStorage
+// Tsy misy Firebase configure amin'izao fotoana izao, fa mampiasa LocalStorage ny tranonkala.
+// Afaka ampifandraisina amin'ny Firebase Firestore ianao rehefa vonona ao amin'ny paramètre.
+console.log('Database Mode: LocalStorage Enterprise Mode Active');
